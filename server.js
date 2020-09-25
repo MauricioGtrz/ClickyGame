0e55c45c2b856f1f2be6a40a26fe5b5ea343b5b6
Mau <mau@pop-os.localdomain>
2020-09-25 02:34:38 -0700

    Added essential files and changed th mongodb url

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,27 +12,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./build"));
 }
 
-app.get("*",(req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clicky-game");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Mauricio:Mauricio@cluster0.8iire.mongodb.net/<dbname>?retryWrites=true&w=majority"
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-=======
-
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, 'build');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
->>>>>>> 05ad694555be390dafe60ae520ae809c95cf851b
 });
 app.listen(port, () => {
-   console.log('Server is up!');
-}); 
+  console.log("Server is up!");
+});
